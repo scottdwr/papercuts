95de9725223ae685716711226c253ae60f221e25
Glitch (hello-express) <none>
2020-05-21 19:27:00 +0000

    👣🤗 Checkpoint

--- papercuts.js
+++ papercuts.js
@@ -27,17 +27,14 @@ async function addSource() {
     {
       text: "Cancel",
       role: "cancel",
-      cssClass: "secondary",
-      handler: () => {
-        console.log("Confirm Cancel");
-      }
+      cssClass: "secondary"
     },
     {
       text: "Add",
-      handler: async (e) => {
+      handler: async e => {
         try {
           await client.addSource(e.url);
-          await refreshSources()
+          await refreshSources();
         } catch (e) {
           error("The URL you entered wasn't a valid source");
         }
@@ -74,25 +71,69 @@ function createSourceListItem(repo) {
   let iio = document.createElement("ion-item-option");
   iio.color = "danger";
   iio.textContent = "Remove";
-  iio.addEventListener("click",()=>{
-    client.removeSource(repo.url)
-  })
+  iio.addEventListener("click", () => {
+    client.removeSource(repo.url);
+    refreshSources();
+  });
   iios.appendChild(iio);
+  return iis;
+}
+
+function createPackageListItem(pkg) {
+  let ii = document.createElement("ion-item");
+  let ia = document.createElement("ion-avatar");
+  ia.slot = "start";
+  let iai = document.createElement("img");
+  iai.src = repo.icon;
+  ia.appendChild(iai);
+  ii.appendChild(ia);
+  let il = document.createElement("ion-label");
+  let ilh = document.createElement("h2");
+  ilh.textContent = repo.name;
+  il.appendChild(ilh);
+  let ilp = document.createElement("p");
+  ilp.textContent = repo.url;
+  il.appendChild(ilp);
+  ii.appendChild(il);
+  iis.appendChild(ii);
+  let iios = document.createElement("ion-item-options");
+  iios.side = "start";
+  iis.appendChild(iios);
+  let iio = document.createElement("ion-item-option");
+  iio.color = "danger";
+  iio.textContent = "Remove";
+  iio.addEventListener("click", () => {
+    client.removeSource(repo.url);
+    refreshSources();
+  });
   return iis;
 }
 
 document.querySelector("#addSource").addEventListener("click", () => {
   addSource();
 });
-window.client=client;
+window.client = client;
 
-const sourceList=document.querySelector("#sourceList");
-async function refreshSources(){
-  await client.init();
-  sourceList.innerHTML="";
-  client.getDb().repos.forEach(e=>{
-    sourceList.appendChild(createSourceListItem(e))
-  })
+const sourceList = document.querySelector("#sourceList");
+
+const wait=ms=>new Promise(c=>setTimeout(c,ms))
+
+async function refreshSources() {
+  const loading = document.createElement("ion-loading");
+  loading.message = "Loading sources...";
+  document.body.appendChild(loading);
+  await Promise.all([
+    loading.present(),
+    (async () => {
+      await client.init();
+      sourceList.innerHTML = "";
+      client.getDb().repos.forEach(e => {
+        sourceList.appendChild(createSourceListItem(e));
+      });
+    })(),
+    wait(1000)
+  ]);
+  await loading.dismiss();
 }
 
-refreshSources()
+refreshSources();
